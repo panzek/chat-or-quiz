8ff5fd9f0f61a350fb7c820707d80b0aef40d9b8
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2024-05-16 12:59:41 +0100

    Delete admin folder and update settings

--- chat_or_quiz/settings.py
+++ chat_or_quiz/settings.py
@@ -31,8 +31,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG =  True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['chatorquiz.ipanzek.com', 'localhost', '127.0.0.1', '67.222.140.61']
 
